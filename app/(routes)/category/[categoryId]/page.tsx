2c97edcf755f1908f7b700d643ea6e43c5f338fe
imonaar <kevinmonari22@gmail.com>
2024-01-03 18:51:22 +0300

    desktop filters complete

---
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -0,0 +1,62 @@
+import { getColors } from "@/actions/get-colors";
+import { getCategory } from "@/actions/get-category";
+import { getProducts } from "@/actions/get-products";
+import { getSizes } from "@/actions/get-sizes";
+import Billboard from "@/components/billboard";
+import Container from "@/components/container";
+import Filter from "@/app/(routes)/category/[categoryId]/_components/filter";
+import NoResult from "@/components/no-result";
+import ProductCard from "@/components/product-card";
+
+export const revalidate = 0
+
+
+interface CategoryPageProps {
+    params: {
+        categoryId: string;
+    },
+    searchParams: {
+        colorId: string;
+        sizeId: string;
+    }
+}
+
+export default async function CategoryPage({ params, searchParams }: CategoryPageProps) {
+    const products = await getProducts({
+        categoryId: params.categoryId,
+        colorId: searchParams.colorId,
+        sizeId: searchParams.sizeId
+    })
+
+    const sizes = await getSizes()
+    const colors = await getColors()
+    const category = await getCategory(params.categoryId)
+
+    return (
+        <div className="bg-white">
+            <Container>
+                <Billboard data={category.billboard} />
+                <div className="px-4 sm:px-6 lg:px-8 pb-24">
+                    <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+                        {/* add mobile filters */}
+                        <div className="hidden lg:block">
+                            <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+                            <Filter valueKey="colorId" name="Colors" data={colors} />
+                        </div>
+                        <div className="mt-6 lg:col-span-4 lg:mt-0">
+                            {
+                                products.length === 0 ? <NoResult /> : (
+                                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                                        {
+                                            products.map(item => <ProductCard key={item.id} data={item} />)
+                                        }
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </div>   
+                </div>
+            </Container>
+        </div>
+    )
+}
